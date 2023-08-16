a757539b5c779163badfc62e05dcecc41af7c566
Garry3Fingers <www.vlad78@yandex.ru>
2023-08-16 20:46:00 +0300

    Create tests for class DisplayMove

---
+++ spec/display_move_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require_relative '../lib/display_move'
+require_relative '../lib/display_board'
+require_relative '../lib/coordinate_board'
+
+describe DisplayMove do
+  describe '#change_postion' do
+    subject(:display_board) { DisplayBoard.new }
+    subject(:c_board) { CoordinateBoard.new }
+    subject(:move) { described_class.new(c_board.board, display_board) }
+
+    before do
+      allow(display_board).to receive(:puts)
+      allow(display_board).to receive(:print)
+    end
+
+    context 'when a player makes a move' do
+      it 'changes the position of the piece' do
+        display_board.print_board
+        move.change_position('d7 d5', 'black')
+        expect(display_board.board[3][3]).to eq(" \u265F ".colorize(color: :black, background: :yellow))
+      end
+
+      it 'changes the previuos position to an empty square' do
+        display_board.print_board
+        move.change_position('d7 d5', 'black')
+        expect(display_board.board[1][3]).to eq('   '.colorize(background: :yellow))
+      end
+    end
+  end
+end
