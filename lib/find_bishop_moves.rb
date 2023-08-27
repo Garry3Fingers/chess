b6586c901133d1ffa00a6a30857a5336af855903
Garry3Fingers <www.vlad78@yandex.ru>
2023-08-27 17:30:25 +0300

    Create module FindBishopMoves

---
+++ lib/find_bishop_moves.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+# This module finds and checks the potencial move of the bishop piece.
+# Key method: #check_bishop_postions.
+module FindBishopMoves
+  def first_diagonal(moves, row, column)
+    if row <= 7 && column <= 7
+      moves << [row, column]
+      first_diagonal(moves, row + 1, column + 1)
+    else
+      moves
+    end
+  end
+
+  def second_diagonal(moves, row, column)
+    if row <= 7 && column >= 0
+      moves << [row, column]
+      second_diagonal(moves, row + 1, column - 1)
+    else
+      moves
+    end
+  end
+
+  def third_diagonal(moves, row, column)
+    if row >= 0 && column <= 7
+      moves << [row, column]
+      third_diagonal(moves, row - 1, column + 1)
+    else
+      moves
+    end
+  end
+
+  def four_diagonal(moves, row, column)
+    if row >= 0 && column >= 0
+      moves << [row, column]
+      four_diagonal(moves, row - 1, column - 1)
+    else
+      moves
+    end
+  end
+
+  def add_potencial_moves(row, column)
+    moves = []
+    moves = first_diagonal(moves, row + 1, column + 1)
+    moves = second_diagonal(moves, row + 1, column - 1)
+    moves = third_diagonal(moves, row - 1, column + 1)
+    four_diagonal(moves, row - 1, column - 1)
+  end
+
+  def check_bishop_postions(pontecial_moves, destination_coordinate)
+    pontecial_moves.any? { |arr| arr == destination_coordinate }
+  end
+end
