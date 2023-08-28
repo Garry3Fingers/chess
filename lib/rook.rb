8f9403c1ef93dd61d438e362202ce412d6a077c7
Garry3Fingers <www.vlad78@yandex.ru>
2023-08-28 21:39:28 +0300

    Refactor code

--- lib/rook.rb
+++ lib/rook.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-require_relative 'coordinate_board'
+require_relative 'validate_rook_move'
+require_relative 'find_rook_moves'
 
 # This class represents a rook piece from a chess game.
 # It has a current position. It also checks if the piece can make a move.
@@ -14,11 +15,14 @@ class Rook
     @board = board
   end
 
+  include FindRookMoves
+  include ValidateRookMove
+
   def change_position(destination, positions)
-    pontecial_moves = add_potencial_moves(row_index(position), column_index(position))
+    pontecial_moves = add_rook_potencial_moves(row_index(position), column_index(position))
     destination_coordinate = [row_index(destination), column_index(destination)]
 
-    if check_postions(pontecial_moves, destination_coordinate) && check_pieces(destination, positions)
+    if check_rook_postions(pontecial_moves, destination_coordinate) && check_rook_path(destination, positions)
       @position = destination
     else
       false
@@ -36,46 +40,5 @@ class Rook
       column = arr.index(location)
       return column unless column.nil?
     end
-  end
-
-  def add_potencial_moves(row, column)
-    moves = []
-
-    (row + 1).upto(7) { |num| moves << [num, column] }
-    (row - 1).downto(0) { |num| moves << [num, column] }
-    (column + 1).upto(7) { |num| moves << [row, num] }
-    (column - 1).downto(0) { |num| moves << [row, num] }
-
-    moves
-  end
-
-  def filter_positions(move, positions)
-    if position[0] == move[0]
-      positions.keep_if { |pos| pos[0] == position[0] }
-    else
-      positions.keep_if { |pos| /[a-h]#{move[1]}/.match?(pos) }
-    end
-  end
-
-  def displacing_positions(move, positions)
-    if position < move
-      positions.keep_if { |pos| pos > position && pos < move }
-    else
-      positions.keep_if { |pos| pos < position && pos > move }
-    end
-  end
-
-  def check_pieces(move, positions)
-    positions = filter_positions(move, positions)
-
-    way_pos = displacing_positions(move, positions)
-
-    return false unless way_pos.empty?
-
-    true
-  end
-
-  def check_postions(pontecial_moves, destination_coordinate)
-    pontecial_moves.any? { |arr| arr == destination_coordinate }
   end
 end
