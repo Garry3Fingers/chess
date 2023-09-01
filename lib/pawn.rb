f59211865cb8730199aae2712d45c90b90e9a649
Garry3Fingers <www.vlad78@yandex.ru>
2023-09-01 21:33:06 +0300

    Add move check

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -16,13 +16,10 @@ class Pawn
     @color = color
   end
 
-  def change_position(destination)
-    pontecial_moves = add_potencial_moves(row_index(position), column_index(position))
-    destination_coordinate = [row_index(destination), column_index(destination)]
-
-    if pontecial_moves.any? { |arr| arr == destination_coordinate }
+  def change_position(move, positions)
+    if diagonal_attack(move, positions) || check_potencial_moves(move) && check_pawn_path(move, positions)
       @first_move = false
-      @position = destination
+      @position = move
     else
       false
     end
@@ -63,5 +60,59 @@ class Pawn
     end
 
     moves
+  end
+
+  def check_top_diagonals(move)
+    [
+      "#{(position[0].ord - 1).chr}#{position[1].next}",
+      "#{position[0].next}#{position[1].next}"
+    ].include?(move)
+  end
+
+  def check_bottom_diagonals(move)
+    [
+      "#{(position[0].ord - 1).chr}#{(position[1].ord - 1).chr}",
+      "#{position[0].next}#{(position[1].ord - 1).chr}"
+    ].include?(move)
+  end
+
+  def diagonal_attack(move, positions)
+    return false unless positions.include?(move)
+
+    if color == 'white' && check_top_diagonals(move)
+      true
+    elsif color == 'black' && check_bottom_diagonals(move)
+      true
+    end
+  end
+
+  def check_potencial_moves(move)
+    pontecial_moves = add_potencial_moves(row_index(position), column_index(position))
+    move_coordinate = [row_index(move), column_index(move)]
+    pontecial_moves.any? { |arr| arr == move_coordinate }
+  end
+
+  def filter_positions(move, positions)
+    if position < move
+      positions.keep_if { |pos| pos[0] == move[0] && pos[1] <= move[1] }
+    else
+      positions.keep_if { |pos| pos[0] == move[0] && pos[1] >= move[1] }
+    end
+  end
+
+  def blocking_positions(move, positions)
+    if position < move
+      positions.keep_if { |pos| pos <= move && pos > position  }
+    else
+      positions.keep_if { |pos| pos >= move && pos < position  }
+    end
+  end
+
+  def check_pawn_path(move, positions)
+    positions = filter_positions(move, positions)
+    way_pos = blocking_positions(move, positions)
+    return false unless way_pos.empty?
+
+    true
   end
 end
