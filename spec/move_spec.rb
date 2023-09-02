1be0524e6168333f8476277b5995c82b96e2eb86
Garry3Fingers <www.vlad78@yandex.ru>
2023-09-02 16:33:05 +0300

    Refactor tests

--- spec/move_spec.rb
+++ spec/move_spec.rb
@@ -4,13 +4,15 @@ require_relative '../lib/move'
 require_relative '../lib/create_pieces'
 
 describe Move do
-  pieces = CreatePieces.new.white_pieces
-  subject(:move) { described_class.new(pieces) }
+  white_pieces = CreatePieces.new.white_pieces
+  black_pieces = CreatePieces.new.black_pieces
+  subject(:move) { described_class.new(white_pieces, black_pieces) }
 
   describe '#make_move' do
     context 'when a player makes a move' do
-      it 'returns a hash with the updated value' do
-        expect(move.make_move('a2 a4')[:pawn1].position).to eq('a4')
+      it 'has a hash with updated position' do
+        move.make_move('a2 a4')
+        expect(move.current_player_pieces[:pawn1].position).to eq('a4')
       end
     end
 
