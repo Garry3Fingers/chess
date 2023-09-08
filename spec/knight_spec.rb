13f354e8e2943a062df61f7e7187afc158d8aa19
Garry3Fingers <www.vlad78@yandex.ru>
2023-09-08 20:38:45 +0300

    Refactor code

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -3,19 +3,20 @@
 require_relative '../lib/knight'
 
 describe Knight do
-  describe '#change_position' do
-    board = [
-      %w[a8 b8 c8 d8 e8 f8 g8 h8],
-      %w[a7 b7 c7 d7 e7 f7 g7 h7],
-      %w[a6 b6 c6 d6 e6 f6 g6 h6],
-      %w[a5 b5 c5 d5 e5 f5 g5 h5],
-      %w[a4 b4 c4 d4 e4 f4 g4 h4],
-      %w[a3 b3 c3 d3 e3 f3 g3 h3],
-      %w[a2 b2 c2 d2 e2 f2 g2 h2],
-      %w[a1 b1 c1 d1 e1 f1 g1 h1]
-    ]
-    subject(:knight) { described_class.new('b1', board) }
+  board = [
+    %w[a8 b8 c8 d8 e8 f8 g8 h8],
+    %w[a7 b7 c7 d7 e7 f7 g7 h7],
+    %w[a6 b6 c6 d6 e6 f6 g6 h6],
+    %w[a5 b5 c5 d5 e5 f5 g5 h5],
+    %w[a4 b4 c4 d4 e4 f4 g4 h4],
+    %w[a3 b3 c3 d3 e3 f3 g3 h3],
+    %w[a2 b2 c2 d2 e2 f2 g2 h2],
+    %w[a1 b1 c1 d1 e1 f1 g1 h1]
+  ]
 
+  subject(:knight) { described_class.new('b1', board) }
+
+  describe '#change_position' do
     context 'when a player makes a move' do
       it 'changes position' do
         pos = []
@@ -26,18 +27,6 @@ describe Knight do
   end
 
   describe '#can_make_move?' do
-    board = [
-      %w[a8 b8 c8 d8 e8 f8 g8 h8],
-      %w[a7 b7 c7 d7 e7 f7 g7 h7],
-      %w[a6 b6 c6 d6 e6 f6 g6 h6],
-      %w[a5 b5 c5 d5 e5 f5 g5 h5],
-      %w[a4 b4 c4 d4 e4 f4 g4 h4],
-      %w[a3 b3 c3 d3 e3 f3 g3 h3],
-      %w[a2 b2 c2 d2 e2 f2 g2 h2],
-      %w[a1 b1 c1 d1 e1 f1 g1 h1]
-    ]
-    subject(:knight) { described_class.new('b1', board) }
-
     context 'when a player makes a legal move' do
       it 'returns true' do
         pos = []
