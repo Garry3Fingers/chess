c4781977d923d9d985db8bb165e0a2bb364ef2c8
Garry3Fingers <www.vlad78@yandex.ru>
2023-09-10 17:16:38 +0300

    Refactor for castling

--- lib/round.rb
+++ lib/round.rb
@@ -32,11 +32,10 @@ class Round
   def input
     input = ''
 
-    while /[a-h][1-8] [a-h][1-8]/.match?(input) == false
+    while /(?:castling )?[a-h][1-8] [a-h][1-8]/.match?(input) == false
       puts "\nEnter the position of the piece to move.\nAnd after the space, the place where to move the piece."
       input = gets.chomp
     end
-
     input
   end
 
@@ -52,12 +51,65 @@ class Round
     display_move.change_position(move, color)
   end
 
-  def player_move(player_move, pieces, enemy_pieces, color)
+  def rook_move_left(color, move)
+    if color == 'white'
+      ['h1', "#{(move[0].ord - 1).chr}#{move[1]}"].join(' ')
+    else
+      ['h8', "#{(move[0].ord - 1).chr}#{move[1]}"].join(' ')
+    end
+  end
+
+  def rook_move_right(color, move)
+    if color == 'white'
+      ['a1', "#{move[0].next}#{move[1]}"].join(' ')
+    else
+      ['a8', "#{move[0].next}#{move_arr[1]}"].join(' ')
+    end
+  end
+
+  def display_rook_castling(player_move, color)
+    move_arr = player_move.split(' ')
+
+    move = if move_arr.first < move_arr.last
+             rook_move_left(color, move_arr.last)
+           else
+             rook_move_right(color, move_arr.last)
+           end
+
+    display_move.change_position(move, color)
+  end
+
+  def castling(player_move, pieces, enemy_pieces, color)
+    if perform_castling(pieces, enemy_pieces, player_move) == false
+      puts 'You can\'t make an illegal move. Try again!'
+      player_move(input, pieces, enemy_pieces, color)
+    else
+      display_pos_change(player_move, color)
+      display_rook_castling(player_move, color)
+    end
+  end
+
+  def perform_castling(pieces, enemy_pieces, move)
+    Move.new(pieces, enemy_pieces).castling(move)
+  end
+
+  def standard_move(player_move, pieces, enemy_pieces, color)
     if move(pieces, enemy_pieces, player_move) == false
       puts 'You can\'t make an illegal move. Try again!'
       player_move(input, pieces, enemy_pieces, color)
     else
       display_pos_change(player_move, color)
+    end
+  end
+
+  def player_move(player_move, pieces, enemy_pieces, color)
+    move_arr = player_move.split(' ')
+
+    if move_arr.first == 'castling'
+      move_arr.shift
+      castling(move_arr.join(' '), pieces, enemy_pieces, color)
+    else
+      standard_move(player_move, pieces, enemy_pieces, color)
     end
   end
 
