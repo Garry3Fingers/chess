cf89c0493b9a447e4c84484d2338d1292d8696a8
Garry3Fingers <www.vlad78@yandex.ru>
2023-09-18 20:58:09 +0300

    Refactor class after adding en_passant

--- lib/move.rb
+++ lib/move.rb
@@ -1,25 +1,28 @@
 # frozen_string_literal: true
 
 require_relative 'castling'
+require_relative 'attackable_king'
 
 # An instance of this class performs one player move on the board.
 # If it's an illegal move, it returns false.
 class Move
-  attr_accessor :current_player_pieces, :pieces_other_player
+  attr_accessor :current_player_pieces, :pieces_other_player, :en_passant
 
-  def initialize(current_player_pieces, pieces_other_player)
+  def initialize(current_player_pieces, pieces_other_player, en_passant)
     @current_player_pieces = current_player_pieces
     @pieces_other_player = pieces_other_player
+    @en_passant = en_passant
   end
+
+  include AttackableKing
 
   def make_move(move)
     move_arr = move.split(' ')
     current_player_pos = positions_hash(current_player_pieces)
     pos = all_positions
-    return false if check_move(move_arr, current_player_pos, pos)
+    return false if select_move(move_arr, current_player_pos, pos)
 
     del_piece(move_arr.last.to_sym)
-
     true
   end
 
@@ -30,6 +33,7 @@ class Move
     return false if king_under_attack?(move_arr, pos, current_player_pos)
     return false if perform_castling(move_arr, pos) == false
 
+    en_passant.pawn_container.clear
     true
   end
 
@@ -67,8 +71,12 @@ class Move
     positions.include?(move_arr.first.to_sym) == false || move_arr.uniq.length == 1
   end
 
+  def can_make_move?(start_pos, end_pos, player_pos, pos)
+    current_player_pieces[player_pos[start_pos]].can_make_move?(end_pos, pos) == false
+  end
+
   def move(start_pos, end_pos, player_pos, pos)
-    current_player_pieces[player_pos[start_pos]].change_position(end_pos, pos) == false
+    current_player_pieces[player_pos[start_pos]].change_position(end_pos, pos)
   end
 
   def del_piece(move)
@@ -78,31 +86,26 @@ class Move
     pieces_other_player.delete(positions[move])
   end
 
-  def king?(positions, move_arr)
-    piece_pos = move_arr.first.to_sym
-    return unless positions.key?(piece_pos)
-
-    positions[piece_pos] == :king
-  end
-
-  def king_under_attack?(move, positions, current_player_pos)
-    return unless king?(current_player_pos, move)
-
-    pieces_other_player.each do |key, value|
-      return true if key.start_with?('pawn') && value.can_make_move?(move.last, [move.last])
-      return true if value.can_make_move?(move.last, positions)
-    end
-
-    false
-  end
-
   def check_move(move_arr, current_player_pos, pos)
-    return true if king_under_attack?(move_arr, pos, current_player_pos)
-    return true if check_before_move(current_player_pos, move_arr)
-    return true if current_player_pos.include?(move_arr.last.to_sym)
-    return true if move(move_arr.first.to_sym, move_arr.last, current_player_pos, pos)
+    return false if king_under_attack?(move_arr, pos, current_player_pos)
+    return false if check_before_move(current_player_pos, move_arr)
+    return false if current_player_pos.include?(move_arr.last.to_sym)
+    return false if can_make_move?(move_arr.first.to_sym, move_arr.last, current_player_pos, pos)
 
-    false
+    true
+  end
+
+  def select_move(move_arr, current_player_pos, pos)
+    if check_move(move_arr, current_player_pos, pos)
+      en_passant.look_for_pawn(move_arr, pos)
+      move(move_arr.first.to_sym, move_arr.last, current_player_pos, pos)
+      false
+    elsif en_passant.check_en_passant(move_arr)
+      en_passant.en_passant(move_arr.last)
+      false
+    else
+      true
+    end
   end
 
   def castling_args(move_arr, positions)
