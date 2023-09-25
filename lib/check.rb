ed63a7632cb8d609cb8fd998800b82ef8f2d922b
Garry3Fingers <www.vlad78@yandex.ru>
2023-09-25 21:53:50 +0300

    Create class Check

---
+++ lib/check.rb
@@ -0,0 +1,118 @@
+# frozen_string_literal: true
+
+require 'colorize'
+
+# This class determines whether there is a king in check.
+class Check
+  attr_reader :white_pieces, :black_pieces
+  attr_accessor :check_color
+
+  def initialize(white_pieces, black_pieces)
+    @white_pieces = white_pieces
+    @black_pieces = black_pieces
+    @check_color = ''
+  end
+
+  def check_after_move(color, positions)
+    notify_about_check(color, positions)
+  end
+
+  def check_before_move(move_arr, color, positions)
+    return false unless fake_move(move_arr, color, positions)
+
+    true
+  end
+
+  private
+
+  def find_attacking_piece(king_position, enemy_pieces, positions)
+    enemy_pieces.each do |key, value|
+      return key if key.start_with?('pawn') && value.can_make_move?(king_position, [king_position])
+      return key if value.can_make_move?(king_position, positions)
+    end
+
+    nil
+  end
+
+  def change_check_color(color)
+    @check_color = if color == 'white'
+                     'black'
+                   elsif color == 'black'
+                     'white'
+                   end
+  end
+
+  def notify_message(color, attacking_piece)
+    if color == 'white'
+      puts "White #{attacking_piece} gives check to the black king".colorize(color: :red)
+    elsif color == 'black'
+      puts "Black #{attacking_piece} gives check to the white king".colorize(color: :red)
+    end
+  end
+
+  def notify_about_check(color, positions)
+    attacking_piece = if color == 'white'
+                        find_attacking_piece(black_pieces[:king].position, white_pieces, positions)
+                      elsif color == 'black'
+                        find_attacking_piece(white_pieces[:king].position, black_pieces, positions)
+                      end
+
+    return if attacking_piece.nil?
+
+    change_check_color(color)
+    notify_message(color, attacking_piece)
+  end
+
+  def deep_copy(object)
+    Marshal.load(Marshal.dump(object))
+  end
+
+  def make_fake_move(pieces, move_arr, positions)
+    pieces.each_value do |piece|
+      next unless piece.position == move_arr.first
+      break unless piece.can_make_move?(move_arr.last, positions)
+
+      piece.position = move_arr.last
+      return true
+    end
+
+    false
+  end
+
+  def positions_arr(pieces)
+    positions = []
+
+    pieces.each_value do |value|
+      pos = value.position
+      positions << pos
+    end
+
+    positions
+  end
+
+  def all_positions(white_p, black_p)
+    white = positions_arr(white_p)
+    black = positions_arr(black_p)
+    white + black
+  end
+
+  def fake_move(move_arr, color, positions)
+    if color == 'white'
+      white_pieces_copy = deep_copy(white_pieces)
+      return unless make_fake_move(white_pieces_copy, move_arr, positions)
+
+      new_positions = all_positions(white_pieces_copy, black_pieces)
+      attacking_piece = find_attacking_piece(white_pieces_copy[:king].position, black_pieces, new_positions)
+    elsif color == 'black'
+      black_pieces_copy = deep_copy(black_pieces)
+      return unless make_fake_move(black_pieces_copy, move_arr, positions)
+
+      new_positions = all_positions(white_pieces, black_pieces_copy)
+      attacking_piece = find_attacking_piece(black_pieces_copy[:king].position, white_pieces, new_positions)
+    end
+
+    return if attacking_piece.nil?
+
+    true
+  end
+end
