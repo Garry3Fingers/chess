b64776dde8e1955bec233727cebad9f11ba7339c
Garry3Fingers <www.vlad78@yandex.ru>
2023-10-22 16:38:06 +0300

    include module RowColumnIndex

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -3,6 +3,7 @@
 require_relative 'validate_bishop_move'
 require_relative 'find_bishop_moves'
 require_relative 'deep_copy'
+require_relative 'row_column_index'
 
 # This class represents a bishop piece from a chess game.
 # It has a current position. It also checks if the piece can make a move.
@@ -19,6 +20,7 @@ class Bishop
   include FindBishopMoves
   include ValidateBishopMove
   include DeepCopy
+  include RowColumnIndex
 
   def change_position(move)
     @position = move
@@ -29,18 +31,5 @@ class Bishop
     pontecial_moves = add_bishop_potencial_moves(row_index(position), column_index(position))
     destination_coordinate = [row_index(move), column_index(move)]
     check_bishop_postions(pontecial_moves, destination_coordinate) && check_bishop_path(move, positions)
-  end
-
-  private
-
-  def row_index(location)
-    board.index { |arr| arr.include?(location) }
-  end
-
-  def column_index(location)
-    board.each do |arr|
-      column = arr.index(location)
-      return column unless column.nil?
-    end
   end
 end
