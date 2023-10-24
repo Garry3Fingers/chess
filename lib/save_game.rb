27d6ddd1c47ca07760b4536632ae165cc5899f73
Garry3Fingers <www.vlad78@yandex.ru>
2023-10-24 16:05:42 +0300

    Refactor class

--- lib/save_game.rb
+++ lib/save_game.rb
@@ -1,31 +1,21 @@
 # frozen_string_literal: true
 
-require 'json'
-
 # This class saves the game table.
 class SaveGame
+  attr_reader :data
+
   def initialize(data)
     @data = data
   end
 
   def save
-    save = to_json
+    save = Marshal.dump(data)
     dirname = 'save_files'
     puts "\nEnter a save file name."
     file_name = gets.chomp
-    save_file = File.open("./#{dirname}/#{file_name}.json", 'w')
+    save_file = File.open("./#{dirname}/#{file_name}", 'w')
     save_file.write(save)
     save_file.close
     puts "\nYour game has been saved."
   end
-
-  private
-
-  def to_json(*args)
-    {
-      'json_class' => self.class.name,
-      'data' => @data
-    }.to_json(*args)
-  end
 end
-
